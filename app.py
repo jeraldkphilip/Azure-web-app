c32fe819060579f247eb97dd4da06800e2ea609b
jeraldkphilip <51280831+jeraldkphilip@users.noreply.github.com>
2023-05-20 13:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "hey there, you have reached Jerald's page!"
+    return "hey there, you have reached Jerald's page after edits. most probably it wont work!"
 
 if __name__ == '__main__':
     app.run()
